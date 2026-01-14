a4c98c5129845fc8abec2cc69a54d5030fa6ccc6
explysm <joshualtrost@gmail.com>
2026-01-14 14:10:08 -0600

    bump: bump to v1.0.4

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -75,7 +75,7 @@ const config = {
                 constModules: {
                   globals: {
                     "bunny-build-info": {
-                      version: `"1.0.3"`,
+                      version: `"1.0.4"`,
                     },
                   },
                 },
