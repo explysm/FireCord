d0a7a0a1d02f2ccbc0816fbd83d17e7472d5ffae
explysm <joshualtrost@gmail.com>
2026-01-17 18:40:49 -0600

    chore:

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,4 +1,4 @@
-export const DISCORD_SERVER = "https://discord.gg/nQykFF9Ud6";
+export const DISCORD_SERVER = "https://discord.gg/nWkQmAh3ue";
 export const GITHUB = "https://github.com/explysm/FireCord";
 export const HTTP_REGEX =
   /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
